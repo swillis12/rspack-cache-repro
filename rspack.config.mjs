24563aafe5d6c7dffe1fbd59f7e228355c79b5b0
Sean Willis <35236833+swillis12@users.noreply.github.com>
2026-06-30 12:26:53 -0700

    Initial commit

---
+++ rspack.config.mjs
@@ -0,0 +1,52 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isRunningWebpack = !!process.env.WEBPACK;
+const isRunningRspack = !!process.env.RSPACK;
+if (!isRunningRspack && !isRunningWebpack) {
+  throw new Error("Unknown bundler");
+}
+
+/**
+ * @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
+ */
+const config = {
+  mode: "development",
+  devtool: false,
+  entry: {
+    main: "./src/index",
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  output: {
+    clean: true,
+    path: isRunningWebpack
+      ? path.resolve(__dirname, "webpack-dist")
+      : path.resolve(__dirname, "rspack-dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/,
+        type: "css/auto",
+      },
+    ],
+  },
+  ...(isRunningWebpack
+    ? {
+        experiments: {
+          css: true,
+        },
+      }
+    : {}),
+};
+
+export default config;
